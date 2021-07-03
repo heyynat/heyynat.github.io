3b058b4f5dc54f49970d83b863a9d82c53a16e9d
heyynat <natalifplima@gmail.com>
2021-07-02 22:40:49 -0400

    Criando base do componenente Header

--- heyynat-dev/src/App.js
+++ heyynat-dev/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 import MyProjects from './pages/MyProjects';
 import Profile from './components/Profile';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <header>
+          <Header />
+        </header>
         <Switch>
           <Route exact path="/"><Profile /></Route>
           <Route path="/myprojects"><MyProjects /></Route>
---
+++ heyynat-dev/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Link }from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/myprojects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Header;
