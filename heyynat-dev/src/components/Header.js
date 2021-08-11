abaf0b8d29ab32ab86de5c808bd84f7b06c1dce3
heyynat <natalifplima@gmail.com>
2021-08-11 12:12:17 -0400

    ...

--- heyynat-dev/src/components/Header.js
+++ heyynat-dev/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link }from 'react-router-dom';
 
 const Header = () => {
   return (
-      <nav role="navigation" aria-label="main navigation">
+      <nav aria-label="main navigation">
           <Link to="/" className="link-start link-item">Home</Link>
           <Link to="/about" className="link-item">About</Link>
           <Link to="/myprojects" className="link-item">Projects</Link>
