27311236c7b6fc67cf747e0f52e8e95e92c6fbaa
heyynat <natalifplima@gmail.com>
2021-08-14 18:27:17 -0400

    Correçao Header

--- heyynat-portifolio/src/components/Header.js
+++ heyynat-portifolio/src/components/Header.js
@@ -3,29 +3,29 @@ import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+    <Navbar collapseOnSelect expand="lg" variant="dark"  sticky="top">
       <Container>
       <Navbar.Brand href="/">Heyynat</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav
-            className="mr-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
+          <Navbar
+          className="me-auto"
           >
-            <Nav.Link href="#about">Sobre</Nav.Link>
-            <NavDropdown title="Contato" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="https://github.com/heyynat">GitHub</NavDropdown.Item>
-              <NavDropdown.Item href="https://www.linkedin.com/in/heyynat/">LinkedIn</NavDropdown.Item>
-              <NavDropdown.Item href="mailto:natalifplima@gmail.com">Email</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Projects" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="https://heyynat.github.io/trybewallet">TrybeWallet</NavDropdown.Item>
-              <NavDropdown.Item href="https://heyynat.github.io/trivia">Trivia</NavDropdown.Item>
-              <NavDropdown.Item href="https://heyynat.github.io/app-recipes">App Recipes</NavDropdown.Item>
-            {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-            </NavDropdown>
-          </Nav>
+            <Nav>
+              <Nav.Link href="#about">Sobre</Nav.Link>
+              <NavDropdown title="Contato" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="https://github.com/heyynat">GitHub</NavDropdown.Item>
+                <NavDropdown.Item href="https://www.linkedin.com/in/heyynat/">LinkedIn</NavDropdown.Item>
+                <NavDropdown.Item href="mailto:natalifplima@gmail.com">Email</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Projects" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="https://heyynat.github.io/trybewallet">TrybeWallet</NavDropdown.Item>
+                <NavDropdown.Item href="https://heyynat.github.io/trivia">Trivia</NavDropdown.Item>
+                <NavDropdown.Item href="https://heyynat.github.io/app-recipes">App Recipes</NavDropdown.Item>
+              {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+              </NavDropdown>
+            </Nav>
+          </Navbar>
         </Navbar.Collapse>
       </Container>
     </Navbar>
