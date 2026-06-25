0e807fda781da9944e0aa58065675226b1fd4ab2
Natali Lima <natalifplima@gmail.com>
2026-06-24 21:06:06 -0400

    feat: add shared site content configuration module

---
+++ src/services/config.js
@@ -0,0 +1,50 @@
+export const resumeAbout =
+  'Sou apaixonada por Tecnologia, desenvolvimento web e marcas que entregam soluções inovadoras e ágeis, gerando valor para negócios e transformações no mundo digital. Trabalho e acredito em ações que possam gerar impacto e simplificar a vida das pessoas.';
+
+export const profileImage =
+  'https://avatars.githubusercontent.com/u/54861311?v=4';
+
+export const softSkills = [
+  { skill: 'Comunicação', progress: 70 },
+  { skill: 'Colaboração', progress: 90 },
+  { skill: 'Inteligência emocional', progress: 80 },
+  { skill: 'Adaptabilidade', progress: 80 },
+  { skill: 'Aprender a Aprender', progress: 85 },
+  { skill: 'Pensamento crítico', progress: 60 },
+];
+
+export const hardSkills = [
+  { skill: 'JavaScript', progress: 80 },
+  { skill: 'GitHub', progress: 70 },
+  { skill: 'Metodologias ágeis', progress: 70 },
+  { skill: 'React', progress: 80 },
+  { skill: 'HTML 5', progress: 90 },
+  { skill: 'CSS 3', progress: 70 },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: 'TrybeWallet',
+    storyline: 'Uma carteira de despesas digital, com Redux',
+    image:
+      'https://images.unsplash.com/photo-1512358958014-b651a7ee1773?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    url: 'https://heyynat.github.io/trybewallet',
+  },
+  {
+    id: 2,
+    title: 'Trivia',
+    storyline: 'Um game de perguntas e respostas',
+    image:
+      'https://images.unsplash.com/photo-1468657988500-aca2be09f4c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    url: 'https://heyynat.github.io/trivia',
+  },
+  {
+    id: 3,
+    title: 'Recipes App',
+    storyline: 'App de receitas com Context API',
+    image:
+      'https://images.unsplash.com/photo-1598214886806-c87b84b7078b?ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80',
+    url: 'https://heyynat.github.io/recipes-app',
+  },
+];
